74b470ba09c27e95f53d65b805a9a4cabf35cfdd
Samin010 <saminbashir484@gmail.com>
2024-09-10 23:51:00 +0530

    "Authetication"

--- base/views.py
+++ base/views.py
@@ -2,12 +2,39 @@ from django.shortcuts import render,redirect
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 # rooms=[
 # {'id':1,'name':'Lets learn Django'},
 # {'id':2,'name':'Hello'},
 # {'id':3,'name':'Sup'},
 # ]
+
+def loginPage(request):
+  if request.method=='POST':
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+
+    try:
+      user=User.objects.get(username=username)
+    except:  
+      messages.error(request,'User does not exist')
+      
+    user=authenticate(request,username=username,password=password)
+    if user is not None:
+      login(request,user)
+      return redirect('home')
+    else:
+      messages.error(request,'Username or password not exist')
+  context={}
+  return render(request,'base/login_register.html',context)
+
+def logoutUser(request):
+  logout(request)
+  return redirect('home')
+
 def home(request):
     q=request.GET.get('q')if request.GET.get('q')!=None else ''
     
