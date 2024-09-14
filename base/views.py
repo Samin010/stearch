728d5f1facc68b1504344071dcc9df4d52fa823e
Samin010 <saminbashir484@gmail.com>
2024-09-14 22:12:18 +0530

    "Day6"

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Room,Topic,Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -62,7 +62,7 @@ def home(request):
     q=request.GET.get('q')if request.GET.get('q')!=None else ''
     
     rooms=Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q)| Q(description__icontains=q))
-    topics=Topic.objects.all()
+    topics=Topic.objects.all()[0:5]
     room_count=rooms.count()
     room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
     context={'rooms':rooms,'topics':topics,'room_count': room_count,'room_messages':room_messages}
@@ -140,3 +140,25 @@ def deleteMessage(request,pk):
     return redirect('home')
   return render(request,'base/delete.html',{'obj':message})
 
+
+@login_required(login_url='login')  
+def updateUser(request):
+  user=request.user
+  form=UserForm(instance=user)
+  if request.method == 'POST':
+    form=UserForm(request.POST,instance=user)
+    if form.is_valid():
+      form.save()
+      return redirect('user-profile',pk=user.id)
+  return render(request,'base/update_user.html',{'form':form})
+
+
+def topicsPage(request):
+  q=request.GET.get('q')if request.GET.get('q')!=None else ''
+  topics=Topic.objects.filter(name__icontains=q)
+  return render(request,'base/topics.html',{'topics':topics})
+
+
+def activityPage(request):
+  room_messages=Message.objects.all()
+  return render(request,'base/activity.html',{'room_messages':room_messages})
